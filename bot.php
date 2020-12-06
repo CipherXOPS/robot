b707329b70e514455ac6a1273d2a4bfda5175641
iaranian-pelikan <70199553+iaranian-pelikan@users.noreply.github.com>
2020-12-06 14:33:25 +0330

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,4 @@
 <?php
-	define('BOT_TOKEN', '1409876414:AAE1A_mGInHsgrrrHEgwu-hzcV5-Fj8fK8o');
 	define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 	
 	function apiRequestWebhook($method, $parameters) {
